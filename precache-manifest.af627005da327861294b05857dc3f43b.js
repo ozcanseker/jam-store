3e34896499cedee07f54299626510e41a5836760
ozcanseker <28202665+ozcanseker@users.noreply.github.com>
2020-09-12 11:32:59 +0200

    Updates

---
+++ precache-manifest.af627005da327861294b05857dc3f43b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8ebc6a21b9f901e9e68314d250264f",
+    "url": "/jam-store/index.html"
+  },
+  {
+    "revision": "1f6c3e9f06257acc97a0",
+    "url": "/jam-store/static/css/main.7fa70d33.chunk.css"
+  },
+  {
+    "revision": "9ac787732be7cfa33bd1",
+    "url": "/jam-store/static/js/2.f1bc8e07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jam-store/static/js/2.f1bc8e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6c3e9f06257acc97a0",
+    "url": "/jam-store/static/js/main.3cfb90a3.chunk.js"
+  },
+  {
+    "revision": "71520f0f0059cf7937d9",
+    "url": "/jam-store/static/js/runtime-main.9cd5da6c.js"
+  },
+  {
+    "revision": "f296a8f272c5aac37b891b9d0e18b0bc",
+    "url": "/jam-store/static/media/4f5b590daa3e9c01f61f383599c763ed.f296a8f2.jpg"
+  },
+  {
+    "revision": "ed7ebb8db4eeb3095001b23d108a0a6d",
+    "url": "/jam-store/static/media/JamJar.ed7ebb8d.jpg"
+  },
+  {
+    "revision": "7806525964b27c550cd80cf284e145d7",
+    "url": "/jam-store/static/media/Knipsel.78065259.PNG"
+  },
+  {
+    "revision": "bf45721c702b6f84251a0809d0d02328",
+    "url": "/jam-store/static/media/appelsaus.bf45721c.png"
+  },
+  {
+    "revision": "e51676b86243262c34f75b49b76539d1",
+    "url": "/jam-store/static/media/applewine.e51676b8.png"
+  },
+  {
+    "revision": "d2262a5d5e15da8cec11a186c7b35240",
+    "url": "/jam-store/static/media/background_content.d2262a5d.jpg"
+  },
+  {
+    "revision": "0a460d3999a0d10c99df9e94f2ccc7ef",
+    "url": "/jam-store/static/media/blackberry.0a460d39.jpg"
+  },
+  {
+    "revision": "6cc7d3311b873247aa536348f8802096",
+    "url": "/jam-store/static/media/blackberry_jam.6cc7d331.png"
+  },
+  {
+    "revision": "7864e60fb14b6caebe95f5849666ee60",
+    "url": "/jam-store/static/media/blackberry_jello.7864e60f.png"
+  },
+  {
+    "revision": "5bc91a67f721aa189004398484eb8fe1",
+    "url": "/jam-store/static/media/e7dc440d75bb181e96384e2ce51f74b5.5bc91a67.jpg"
+  },
+  {
+    "revision": "07c2247c1f721058a2e0d6402a08810a",
+    "url": "/jam-store/static/media/images.07c2247c.png"
+  },
+  {
+    "revision": "2111f68b983afb904cc959cd45db5157",
+    "url": "/jam-store/static/media/locationimage.2111f68b.PNG"
+  },
+  {
+    "revision": "862d7a754d0ec382e6836c7ff68e37dc",
+    "url": "/jam-store/static/media/picnic_blanket.862d7a75.jpg"
+  },
+  {
+    "revision": "0a460d3999a0d10c99df9e94f2ccc7ef",
+    "url": "/jam-store/static/media/stawbery jam.0a460d39.jpg"
+  },
+  {
+    "revision": "8668774e7f5f6058f41891985579ec99",
+    "url": "/jam-store/static/media/stawbery jello.8668774e.jpg"
+  }
+]);
